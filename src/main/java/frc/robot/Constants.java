6634086da063fca303bd10f7cdb674d8d694997b
BOB Contributer <bigbadbob@gmail.com>
2025-01-18 14:17:18 -0500

    tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,15 +53,15 @@ public class Constants {
   }
 
   public static class DriveConstants{
-    public static final double MAX_LINEAR_SPEED = Units.feetToMeters(17.3);
-    public static final double TRACK_WIDTH_X = Units.inchesToMeters(22.0);
-    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(22.0);
+    public static final double MAX_LINEAR_SPEED = Units.feetToMeters(17.1); //y
+    public static final double TRACK_WIDTH_X = Units.inchesToMeters(22.0); //y
+    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(22.0); //y
     public static final double DRIVE_BASE_RADIUS =
         Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
     public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
     public static final int currentLimit = 40; // TODO consider setting this to 60
     public static final double wheelRadiusMeters = Units.inchesToMeters(3.875); // Black nitrile : 3 7/8 with full tread = 3.875 inches
-    public static final double motorReduction = 6.122; // Check me - EKM 11/11
+    public static final double motorReduction = 6.122; //y
     public static final double robotMassKg = 74.088; // TODO
     public static final double robotMOI = 6.883; // TODO
     public static final double wheelCOF = 1.2; // TODO
