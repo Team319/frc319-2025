df597d076e9e8ca0f82e455c28ae711430aa9f91
BOB Contributer <bigbadbob@gmail.com>
2025-01-25 14:06:22 -0500

    tuning and making paths to tune odometry

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -93,10 +93,10 @@ public class Robot extends LoggedRobot {
 
     allianceColor.ifPresent(alliance -> {
       if (alliance == Alliance.Red) {
-        m_robotContainer.drive.setHeading(0.0);
+        //m_robotContainer.drive.setHeading(0.0);
         System.out.println("Red Alliance: Setting heading to 0 degrees.");
       } else if (alliance == Alliance.Blue) {
-        m_robotContainer.drive.setHeading(180.0);
+        //m_robotContainer.drive.setHeading(180.0);
         System.out.println("Blue Alliance: Setting heading to 180 degrees.");
       }
     });
