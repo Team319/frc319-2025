27456135e5f6cc945323f8405812464a22e3b6ff
FRC 319 <frc319.git@gmail.com>
2025-02-24 15:16:48 -0500

    all but 3 commands made

--- src/main/java/frc/robot/commands/GoHome.java
+++ src/main/java/frc/robot/commands/GoHome.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.Constants.AlgaePivotConstants;
 import frc.robot.Constants.CoralPivotConstants;
 import frc.robot.Constants.ElevatorConstants;
--- src/main/java/frc/robot/commands/ScoreL4.java
+++ src/main/java/frc/robot/commands/ScoreL4.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.Constants.CoralPivotConstants;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.subsystems.coralPivot.CoralPivot;
@@ -42,7 +41,7 @@ public class ScoreL4 extends Command{
         }
         passedCycles++;
     }
-    
+
     @Override
     public void end(boolean interrupted) {
         coralPivot.stop();
